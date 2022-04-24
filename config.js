c8d905b1dc41932cd5096414ed876aa486f1b8cb
Maki <53591141+ShikinamiTypeMaki@users.noreply.github.com>
2022-04-24 17:17:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export default {
   githubRepo: 'nuxt2-blog', 
   githubBranch: 'master', 
   domain: 'https://blog.halberd.cn', 
-  SEO_description: '简单的个人博客',
+  SEO_description: 'Maki的个人博客',
   SEO_keywords: 'ShikinamiTypeMaki-blog',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
